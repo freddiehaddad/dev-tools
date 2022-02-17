1122d85ad2066cf33c48e09d3353c453562d8fa2
Freddie Haddad <freddie.haddad@tanium.com>
2022-02-17 08:43:43 -0800

    fix bug in lsp.lua not returning on error

--- nvim/lua/user/plugins/lsp.lua
+++ nvim/lua/user/plugins/lsp.lua
@@ -3,6 +3,7 @@ local status_ok, plugin
 status_ok, plugin = pcall(require, "nvim-lsp-installer")
 if not status_ok then
   vim.notify("Plugin: nvim-lsp-installer not found!")
+  return
 end
 
 plugin.on_server_ready(function(server)
