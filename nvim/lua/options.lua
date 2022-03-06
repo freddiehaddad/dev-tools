b5b752bd19bb55cd89d19865adcb8200032018e4
Freddie Haddad <freddie.haddad@outlook.com>
2022-03-06 05:19:45 -0800

    add additional default options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,13 +1,25 @@
 local M = {}
 
 local options = {
+  clipboard = "unnamedplus",
   cursorline = true,
+  expandtab = true,
+  fileencoding = "utf-8",
   fillchars = 'eob: ',
   number = true,
+  numberwidth = 4,
   pumblend = 10,
   pumheight = 20,
   relativenumber = true,
+  scrolloff = 4,
+  shiftwidth = 2,
+  showmode = false,
+  sidescrolloff = 4,
+  signcolumn = "yes",
+  splitbelow = true,
+  splitright = true,
   swapfile = false,
+  tabstop = 2,
   termguicolors = true,
   undofile = true,
 }
